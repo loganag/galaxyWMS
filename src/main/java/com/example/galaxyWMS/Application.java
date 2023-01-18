d1146dd662edccfe78f33fd64b3b82e8dc0d607a
LoganAG <leha.pushkar@gmail.com>
2023-01-18 22:22:18 +0300

    Added goods entity class

--- src/main/java/com/example/galaxyWMS/Application.java
+++ src/main/java/com/example/galaxyWMS/Application.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GalaxyWmsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GalaxyWmsApplication.class, args);
-	}
-
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
