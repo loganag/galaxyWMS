e68d9924c2162b76b93c80b810c5e999a13c15db
LoganAG <leha.pushkar@gmail.com>
2023-01-18 19:28:55 +0300

    Initial commit

---
+++ src/main/java/com/example/galaxyWMS/GalaxyWmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.galaxyWMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxyWmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaxyWmsApplication.class, args);
+	}
+
+}
